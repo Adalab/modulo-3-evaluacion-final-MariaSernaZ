95348d01539ba53c94ec5ab12ef7f08edf560cbf
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-06-15 20:22:00 +0200

    Add logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import getDataFromApi from "../services/api";
 import CharacterList from "./CharacterList";
+import logoHeader from "../images/Rick&MortyLogo.png";
 import "../stylesheets/App.scss";
 
 function App() {
@@ -15,8 +16,16 @@ function App() {
 
   return (
     <>
-      <h1>Rick and Morty</h1>
-      <CharacterList characters={characters} />
+      <header>
+        <img
+          src={logoHeader}
+          alt="Rick and Morty logo"
+          title="Rick and Morty logo"
+        />
+      </header>
+      <main>
+        <CharacterList characters={characters} />
+      </main>
     </>
   );
 }
